c83a811bac82ff909c736f13ae0687b383e02012
Julia Hong <hong0747@umn.edu>
2026-04-27 11:09:51 -0500

    Data import and cleaning

---
+++ R/week13-dplyr.R
@@ -0,0 +1,35 @@
+# Script Settings and Resources
+setwd(dirname(rstudioapi::getActiveDocumentContext()$path)) 
+library(tidyverse)
+library(DBI)
+library(RPostgres)
+library(usethis)
+
+# Data Import and Cleaning
+conn <- DBI::dbConnect(
+  RPostgres::Postgres(),
+  user = Sys.getenv("NEON_USER"),
+  password = Sys.getenv("NEON_PW"),
+  dbname = "neondb",
+  host = "ep-billowing-union-am14lcnh-pooler.c-5.us-east-1.aws.neon.tech",
+  port = 5432,
+  sslmode = "require")
+
+dbListTables(conn) # Find out which tables I have access to
+
+## Download data science datasets
+employees_tbl <- dbGetQuery(conn, "SELECT * FROM datascience_employees")
+testscores_tbl <- dbGetQuery(conn, "SELECT * FROM datascience_testscores")
+offices_tbl <- dbGetQuery(conn, "SELECT * FROM datascience_offices")
+
+## Save as .csv
+write_csv(employees_tbl, "../data/employees.csv")
+write_csv(testscores_tbl, "../data/testscores.csv")
+write_csv(offices_tbl, "../data/offices.csv")
+
+## Combine data such that employees without test scores are removed
+week13_tbl <- employees_tbl %>%
+  innerjoin(testscores_tbl, by = "employee_id") %>%
+  left_join(offices_tbl, by = c("city" = "office"))
+
+write_csv(week12_tbl, "../data/week13.csv")
